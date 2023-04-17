13039dcd7bdef9026d63d1c289d89803dcc387ab
Steven Lee <stevelee83@gmail.com>
2023-04-17 12:13:45 -0700

    add code for count_up and in_range

--- in_range.py
+++ in_range.py
@@ -16,6 +16,8 @@ def in_range(nums, lowest, highest):
     """
 
     # YOUR CODE HERE
+    for num in nums:
+        if num in range(lowest, highest+1):
+            print(f"{num} fits")
 
-
-in_range([10, 20, 30, 40, 50], 15, 30)            
+in_range([10, 20, 30, 40, 50], 15, 30)
